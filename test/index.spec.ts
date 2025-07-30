6cd0301983ad9d06f192ab0a122dc3e2d7dcdce6
Guilherme Scotti <glscotti@hotmail.com>
2025-07-30 19:56:51 -0300

    test: put tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,6 +2,7 @@ import { KeyValueCache, KeyValueCacheAdapter } from "../src";
 
 type TestParams = { id: string };
 
+const MOCK_PREFIX = "prefix";
 const MOCK_FILE_PATH = "/mock/path/image.jpg";
 const MOCK_FILE_DOES_NOT_EXIST_PATH = "/does/not/exist/image.jpg";
 const MOCK_NOT_FOUND_KEY = "not-found";
@@ -22,7 +23,7 @@ describe("KeyValueCache", () => {
 
   beforeEach(() => {
     adapter = {
-      prefix: "prefix",
+      prefix: MOCK_PREFIX,
       evictionMillis: EVICTION_MILLIS,
       maxEntries: MAX_ENTRIES,
       maxCacheSize: MAX_CACHE_SIZE,
@@ -186,5 +187,95 @@ describe("KeyValueCache", () => {
     expect(keys).toContain(key);
     // eslint-disable-next-line @typescript-eslint/unbound-method
     expect(adapter.fileUnlink).not.toHaveBeenCalled();
+  });
+
+  it("put awaits to boot", async () => {
+    let getAllKeysTimestamp = 0;
+    let getKeyForTimestamp = 0;
+
+    let signalResolve!: () => void;
+    const signal = new Promise<void>(resolve => {
+      signalResolve = resolve;
+    });
+
+    adapter.getAllKeys = jest.fn().mockImplementation(async () => {
+      await signal;
+      getAllKeysTimestamp = getTimestamp();
+      console.log("getAllKeysTimestamp", getAllKeysTimestamp);
+      return [];
+    });
+
+    adapter.getKeyFor = jest
+      .fn()
+      .mockImplementation(async (_params: TestParams) => {
+        getKeyForTimestamp = getTimestamp();
+        console.log("getKeyForTimestamp", getKeyForTimestamp);
+        const result = await Promise.resolve(null);
+        return result;
+      });
+
+    const newCache = new KeyValueCache(adapter); // starts boot
+    const putPromise = newCache.put({ id: "test" }, MOCK_FILE_PATH);
+
+    signalResolve();
+
+    const result = await putPromise;
+
+    expect(result).toBe(false);
+    expect(getAllKeysTimestamp).toBeGreaterThan(0);
+    expect(getKeyForTimestamp).toBeGreaterThan(0);
+    expect(getKeyForTimestamp).toBeGreaterThan(getAllKeysTimestamp);
+  });
+
+  it("put returns false if key is falsy", async () => {
+    const result = await cache.put({ id: MOCK_NOT_FOUND_KEY }, MOCK_FILE_PATH);
+    expect(result).toBe(false);
+  });
+
+  it("put returns true when all conditions are met", async () => {
+    const key = "TheKey";
+    const result = await cache.put({ id: key }, MOCK_FILE_PATH);
+    expect(result).toBe(true);
+    expect(dictionary[key]).toContain(`"filePath":"${MOCK_FILE_PATH}"`);
+    expect(dictionary[key]).toContain('"lastAccessed"');
+  });
+
+  it("put cleans up count when maxEntries is reached", async () => {
+    const lastAccessed = Date.now() - EVICTION_MILLIS - 1;
+    const value = `{ "filePath": "${MOCK_FILE_PATH}", "lastAccessed": ${lastAccessed} }`;
+    const oldKey = `${MOCK_PREFIX}:OldKey`;
+    dictionary[oldKey] = value;
+
+    adapter.maxEntries = 1;
+    const newCache = new KeyValueCache(adapter);
+
+    const newKey = `${MOCK_PREFIX}:NewKey`;
+    const result = await newCache.put({ id: newKey }, MOCK_FILE_PATH);
+    expect(result).toBe(true);
+    expect(dictionary[newKey]).toContain(`"filePath":"${MOCK_FILE_PATH}"`);
+    expect(dictionary[newKey]).toContain('"lastAccessed"');
+    expect(dictionary[oldKey]).toBeUndefined();
+  });
+
+  it("put cleans up disk when maxCacheSize is reached", async () => {
+    const lastAccessed = Date.now() - EVICTION_MILLIS - 1;
+    const value = `{ "filePath": "${MOCK_FILE_PATH}", "lastAccessed": ${lastAccessed} }`;
+    const oldKey = `${MOCK_PREFIX}:OldKey`;
+    dictionary[oldKey] = value;
+
+    adapter.maxCacheSize = 1000;
+    adapter.fileSize = jest
+      .fn()
+      .mockImplementation(async (_path: string): Promise<number> => {
+        return Promise.resolve(1000);
+      });
+    const newCache = new KeyValueCache(adapter);
+
+    const newKey = `${MOCK_PREFIX}:NewKey`;
+    const result = await newCache.put({ id: newKey }, MOCK_FILE_PATH);
+    expect(result).toBe(true);
+    expect(dictionary[newKey]).toContain(`"filePath":"${MOCK_FILE_PATH}"`);
+    expect(dictionary[newKey]).toContain('"lastAccessed"');
+    expect(dictionary[oldKey]).toBeUndefined();
   });
 });
